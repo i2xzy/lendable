a072c4b771bd7cdf8892795e848c5e590dc62a9c
i2xzy <yitzchokr@live.com>
2019-01-15 17:57:41 +0000

    style Main component, import and render QuoteForm

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import QuoteForm from './components/QuoteForm.js';
 
-export default App;
+const Main = styled.main`
+  text-align: center;
+  background-color: #0d1337;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default () => (
+  <Main>
+    <QuoteForm />
+  </Main>
+);
